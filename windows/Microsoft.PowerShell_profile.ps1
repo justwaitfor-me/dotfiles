ff803136e8535162eca658cc14465e177e8f2971
Just Wait <reimdiego@icloud.com>
2024-07-24 02:06:48 +0200

    init

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+﻿
+#Import-Module posh-git
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+function fortune {
+[System.IO.File]::ReadAllText((Split-Path $profile)+'\fortune.txt') -replace "`r`n", "`n" -split "`n%`n" | Get-Random
+}
+
+Invoke-Expression (&starship init powershell)
+
+neofetch
+
+fortune; cowsay
+
+
